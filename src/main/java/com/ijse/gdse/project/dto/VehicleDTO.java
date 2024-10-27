348a7f0021cdca4cf9daf8418378a96e1c1cc2f2
Rishithaw <rishithaw1@gmail.com>
2024-10-27 22:30:48 +0530

    commit

---
+++ src/main/java/com/ijse/gdse/project/dto/VehicleDTO.java
@@ -0,0 +1,17 @@
+package com.ijse.gdse.project.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class VehicleDTO {
+    private int vec_id;
+    private String vehicle_type;
+    private String lesson_fee;
+    private String ad_id;
+    private String in_id;
+    private String mec_id;
+}
---
+++ src/main/java/com/ijse/gdse/project/dto/tm/VehicleTM.java
@@ -0,0 +1,17 @@
+package com.ijse.gdse.project.dto.tm;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class VehicleTM {
+    private int vec_id;
+    private String vehicle_type;
+    private String lesson_fee;
+    private String ad_id;
+    private String in_id;
+    private String mec_id;
+}
