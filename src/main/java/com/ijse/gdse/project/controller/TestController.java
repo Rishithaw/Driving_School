3e2c149002e199f1ecec942b6a9c9c18f2e323b0
Rishithaw <rishithaw1@gmail.com>
2024-10-27 22:30:13 +0530

    commit

--- src/main/java/com/ijse/gdse/project/controller/TestController.java
+++ src/main/java/com/ijse/gdse/project/controller/TestController.java
@@ -1,4 +1,85 @@
 package com.ijse.gdse.project.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
 public class TestController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private Circle cir;
+
+    @FXML
+    private TableColumn<?, ?> colDate;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colInstructor;
+
+    @FXML
+    private TableColumn<?, ?> colStID;
+
+    @FXML
+    private TableColumn<?, ?> colStName;
+
+    @FXML
+    private TableColumn<?, ?> colTime;
+
+    @FXML
+    private DatePicker dpTestDate;
+
+    @FXML
+    private ImageView imgProfile;
+
+    @FXML
+    private Label lblID;
+
+    @FXML
+    private Label lblID1;
+
+    @FXML
+    private TableView<?> tblTest;
+
+    @FXML
+    private TextField txtInstructor;
+
+    @FXML
+    private TextField txtStName;
+
+    @FXML
+    private TextField txtTime;
+
+    @FXML
+    void SaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/com/ijse/gdse/project/controller/VehicleController.java
+++ src/main/java/com/ijse/gdse/project/controller/VehicleController.java
@@ -1,4 +1,84 @@
 package com.ijse.gdse.project.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+
 public class VehicleController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private Circle cir;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colInstructor;
+
+    @FXML
+    private TableColumn<?, ?> colLessonFee;
+
+    @FXML
+    private TableColumn<?, ?> colMechanic;
+
+    @FXML
+    private TableColumn<?, ?> colVehicleType;
+
+    @FXML
+    private ImageView imgProfile;
+
+    @FXML
+    private Label lblID;
+
+    @FXML
+    private TableView<?> tblVehicle;
+
+    @FXML
+    private TextField txtInstructor;
+
+    @FXML
+    private TextField txtLessonFee;
+
+    @FXML
+    private TextField txtMechanic;
+
+    @FXML
+    private TextField txtVehicle;
+
+    @FXML
+    void SaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTable(MouseEvent event) {
+
+    }
+
+    @FXML
+    void updateOnAction(ActionEvent event) {
+
+    }
+
 }
